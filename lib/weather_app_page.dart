dd86f7b7b4c8fdf9b913f39fbc9753e0ba1f78c9
Sersei7 <kaduaniket718@gmail.com>
2025-02-09 17:20:28 +0530

    Project Setup done

---
+++ lib/weather_app_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherAppPage extends StatefulWidget {
+  const WeatherAppPage({super.key});
+
+  @override
+  State<WeatherAppPage> createState() => _WeatherAppPageState();
+}
+
+class _WeatherAppPageState extends State<WeatherAppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
